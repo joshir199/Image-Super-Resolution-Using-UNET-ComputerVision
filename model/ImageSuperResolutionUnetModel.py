cfd53aadbb23401c45134548f8a80ae2faaf4f74
Raushan Joshi <raushanjoshi199@gmail.com>
2023-10-19 14:14:47 +0530

    created UNET model class and its architecture

---
+++ model/ImageSuperResolutionUnetModel.py
@@ -0,0 +1,71 @@
+import tensorflow as tf
+
+
+class ImageSuperResolutionUnetModel:
+
+    def __init__(self, SIZE, channels, isTraining=True):
+        """
+        :param SIZE: Image size
+        :param channels: number of channels in input images
+        :param isTraining: is training or not
+        """
+        self.model_name = 'UNet Model'
+        self.isTraining = isTraining
+        self.SIZE = SIZE
+        self.channels = channels
+        self.input = tf.keras.layers.Input(shape=(self.SIZE, self.SIZE, self.channels))
+        self.conv2d_1 = tf.keras.layers.Conv2D(self.channels, kernel_size=(1, 1), padding='same',
+                                               name='UNet Model/Intput/Conv_1')
+        self.output = tf.keras.layers.Conv2D(self.channels, kernel_size=(1, 1), padding='same',
+                                             name='UNet model/output/Conv_out')
+
+    def build_model(self):
+        """
+        :return: returns UNET model
+        """
+
+        input = self.input
+        x = self.conv2d_1(input)
+
+        skip_1, x = self.encoder_block(x, 32, 'encode_1', self.model_name)
+        skip_2, x = self.encoder_block(x, 64, 'encode_2', self.model_name)
+        skip_3, x = self.encoder_block(x, 128, 'encode_3', self.model_name)
+        skip_4, x = self.encoder_block(x, 256, 'encode_4', self.model_name)
+        skip_5, x = self.encoder_block(x, 512, 'encode_5', self.model_name)
+
+        bottom = self.conv_block(x, 1024, 'bottom', self.model_name)
+
+        d1 = self.decoder_block(bottom, 512, skip_5, 'decode_1')
+        d2 = self.decoder_block(d1, 256, skip_4, 'decode_2')
+        d3 = self.decoder_block(d2, 128, skip_3, 'decode_3')
+        d4 = self.decoder_block(d3, 64, skip_2, 'decode_4')
+        d5 = self.decoder_block(d4, 32, skip_1, 'decode_5')
+
+        outputs = self.output(d5)
+        model = tf.keras.models.Model(input, outputs)
+
+        return model
+
+    def conv_block(self, input_features, num_filters, block_name, model_name):
+        x = tf.keras.layers.Conv2D(num_filters, kernel_size=(3, 3), padding='same',
+                                   name=model_name + '/' + block_name + '/conv_1')(input_features)
+        x = tf.keras.layers.BatchNormalization(trainable=self.isTraining)(x)
+        x = tf.keras.layers.LeakyReLU()(x)
+
+        x = tf.keras.layers.Conv2D(num_filters, kernel_size=(3, 3), padding='same',
+                                   name=model_name + '/' + block_name + '/conv_1')(x)
+        x = tf.keras.layers.BatchNormalization(trainable=self.isTraining)(x)
+        x = tf.keras.layers.LeakyReLU()(x)
+        return x
+
+    def encoder_block(self, input_features, num_filters, block_name, model_name):
+        skip_layer = self.conv_block(input_features, num_filters, block_name, model_name)(input_features)
+        x = tf.keras.layers.MaxPool2D(pool_size=(2, 2))(skip_layer)
+        return skip_layer, x
+
+    def decoder_block(self, input_features, num_filters, skip_layer, block_name):
+        x = tf.keras.layers.Conv2DTranspose(num_filters, kernel_size=(2, 2), strides=(2, 2),
+                                            name=self.model_name + '/' + block_name + '/convTranspose_1')(input_features)
+        x = tf.keras.layers.Concatenate()([x, skip_layer])
+        x = tf.keras.layers.Dropout(0.3)(x)
+        return x
